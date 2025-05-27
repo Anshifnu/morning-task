ad216360fc540b615b0bc9a0d0727113bc7deff5
anshif <your@email.com>
2025-05-27 12:52:45 +0530

    divisible-by-3

---
+++ work1.js
@@ -0,0 +1,14 @@
+const items=[1,24,3,4,53,6,72,8,92,10];
+for( i=0;i<items.length;i++){
+    if (items[i]%3!==0) {
+        
+       console.log(items[i]);
+        
+        
+        
+    } 
+    
+
+}
+
+
